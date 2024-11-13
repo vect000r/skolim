a0cc22e6b98304775921a5effe7f89a30180978b
vect0r <vectoreddd@gmail.com>
2024-11-13 01:01:45 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,4 @@
+import skolim
+
+if __name__ == '__main__':
+    skolim.run_bot()
---
+++ skolim.py
@@ -0,0 +1,45 @@
+import discord
+import os
+import asyncio
+import yt_dlp
+from dotenv import load_dotenv
+
+def run_bot():
+    load_dotenv()
+    AUTH_TOKEN = os.getenv('discord_token')
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents=intents)
+
+    voice_clients = {}
+    yt_dl_options = {"format": "bestaudio/best"}
+    ytdl = yt_dlp.YoutubeDL(yt_dl_options)
+
+    ffmpeg_options = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5','options': '-vn -filter:a "volume=0.25"'}
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now jamming')
+
+    @client.event
+    async def on_message(message):
+        if message.content.startswith("?play"):
+            try:
+                voice_client = await message.author.voice.channel.connect()
+                voice_clients[voice_client.guild.id] = voice_client
+            except Exception as err:
+                print(err)
+            
+            try:
+                url = message.content.split()[1]
+
+                loop = asyncio.get_event_loop()
+                data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=False))
+
+                song = data['url']
+                player = discord.FFmpegOpusAudio(song, **ffmpeg_options)
+                
+                voice_clients[message.guild.id].play(player)
+            except Exception as err:
+                print(err)
+    client.run(AUTH_TOKEN)
