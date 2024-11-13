2926777dbbada6777b9cf04fc58d39d8d9b87044
vect00r <vectoreddd@gmail.com>
2024-11-13 20:49:12 +0100

    skolim restructurized

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import skolim
 
 if __name__ == '__main__':
+    skolim = skolim.Skolim()
     skolim.run_bot()
--- skolim.py
+++ skolim.py
@@ -4,42 +4,58 @@ import asyncio
 import yt_dlp
 from dotenv import load_dotenv
 
-def run_bot():
-    load_dotenv()
-    AUTH_TOKEN = os.getenv('discord_token')
-    intents = discord.Intents.default()
-    intents.message_content = True
-    client = discord.Client(intents=intents)
+class Skolim:
+    def __init__(self):
+        load_dotenv()
+        self.AUTH_TOKEN = os.getenv('discord_token')
+        self.intents = discord.Intents.default()
+        self.intents.message_content = True
+        self.client = discord.Client(intents=self.intents)
+        self.voice_clients = {}
+        self.yt_dl_options = {"format": "bestaudio/best"}
+        self.ytdl = yt_dlp.YoutubeDL(self.yt_dl_options)
+        self.ffmpeg_options = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5','options': '-vn -filter:a "volume=0.25"'}
 
-    voice_clients = {}
-    yt_dl_options = {"format": "bestaudio/best"}
-    ytdl = yt_dlp.YoutubeDL(yt_dl_options)
+    def run_bot(self):
+        @self.client.event
+        async def on_ready():
+            print(f'{self.client.user} is now jamming')
 
-    ffmpeg_options = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5','options': '-vn -filter:a "volume=0.25"'}
-
-    @client.event
-    async def on_ready():
-        print(f'{client.user} is now jamming')
-
-    @client.event
-    async def on_message(message):
-        if message.content.startswith("?play"):
-            try:
-                voice_client = await message.author.voice.channel.connect()
-                voice_clients[voice_client.guild.id] = voice_client
-            except Exception as err:
-                print(err)
+        @self.client.event
+        async def on_message(message):
+            if message.content.startswith("?play"):
+                try:
+                    voice_client = await message.author.voice.channel.connect()
+                    self.voice_clients[voice_client.guild.id] = voice_client
+                except Exception as err:
+                    print(err)
             
-            try:
-                url = message.content.split()[1]
+                try:
+                    url = message.content.split()[1]
 
-                loop = asyncio.get_event_loop()
-                data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=False))
+                    loop = asyncio.get_event_loop()
+                    data = await loop.run_in_executor(None, lambda: self.ytdl.extract_info(url, download=False))
 
-                song = data['url']
-                player = discord.FFmpegOpusAudio(song, **ffmpeg_options)
+                    song = data['url']
+                    player = discord.FFmpegOpusAudio(song, **self.ffmpeg_options)
                 
-                voice_clients[message.guild.id].play(player)
-            except Exception as err:
-                print(err)
-    client.run(AUTH_TOKEN)
+                    self.voice_clients[message.guild.id].play(player)
+                except Exception as err:
+                    print(err)
+        self.client.run(self.AUTH_TOKEN)
+    
+
+    def make_queue(self):
+        pass
+        #TODO
+    
+
+    def skip(self):
+        pass
+        #TODO
+
+
+    def stop(self):
+        pass
+        #TODO
+    
